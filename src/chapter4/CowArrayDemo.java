e460e631fc87b0c26fc6bca5d6c052c00f0b725a
HaiNguyen <nguyenlehai@hotmail.com>
2020-06-09 09:17:49 +0700

    Naming of CowArrayDemo.java

---
+++ src/chapter4/CowArrayDemo.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+public class CowArrayDemo {
+  public static void main(String [] args) {
+	for (String arg : args) {
+	  System.out.println(arg);
+	}
+  }
+}
+
+/*How to run on cmd:
+1, javac CowArrayDemo.java
+2, java CowArrayDemo foo bar
+Có thể truyền những tham số khác vào sau đoạn "java CowArrayDemo"
+ */
