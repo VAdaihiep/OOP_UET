d452ffe9b55ca0a7a020a57bca2e3ef5c9a3cc53
HaiNguyen <nguyenlehai@hotmail.com>
2020-06-10 17:30:36 +0700

    Fix wrong answer in chapter 6.

--- src/chapter6/Dice.java
+++ src/chapter6/Dice.java
@@ -1,13 +1,15 @@
 package chapter6;
 
 public class Dice {
+  int status = 0;
+
   public int roll() {
 	return (int) (Math.random() * 6) + 1;
   }
 
   public static void main(String[] args) {
 	Dice dice = new Dice();
-	int saveDice = dice.roll();
-	System.out.println(saveDice);
+	dice.status = dice.roll();
+	System.out.println(dice.status);
   }
 }
--- src/chapter6/cardTestDrive/CardSet.java
+++ src/chapter6/cardTestDrive/CardSet.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class CardSet {
+  ArrayList<Card> shuffleCard = new ArrayList<>();
+
   void shuffle() {
 	int k = 0;
-	ArrayList<String> shuffleCard = new ArrayList<>();
 	for (int i = 0; i < Card.RANK.length; i++) {
 	  for (int j = 0; j < Card.SUITS.length; j++) {
 		Card card = new Card(Card.RANK[i], Card.SUITS[j]);
-		shuffleCard.add(k, (card.rank + "-" + card.suit + "\n"));
+		shuffleCard.add(k, (card));
 		Collections.shuffle(shuffleCard);
 	  }
 	}
-	for (Object o : shuffleCard) {
-	  System.out.println(o);
+	for (Card card : shuffleCard) {
+	  System.out.println(card.rank + "-" + card.suit);
 	}
   }
 }
